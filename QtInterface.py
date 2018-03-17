953590c1e87cf497117366708ad5edf63649685d
nicetodoitagain <>
2018-03-17 21:36:02 +0200

    Added eids and cumulate plots

--- QtInterface.py
+++ QtInterface.py
@@ -6,63 +6,111 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-#TODO: Додати графіки
+# TODO: Додати графіки
 
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QPushButton, QScrollArea, QTableWidget, QDoubleSpinBox, QTextEdit
 from PyQt5.QtWidgets import QSizePolicy, QVBoxLayout, QHBoxLayout
-from PyQt5.QtWidgets import QWidget,QMainWindow,QApplication
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QTableWidgetItem
 
 import sys
 import Lab1
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib import pyplot as plt
+
+from numpy import arange
+from numpy import unique
+
+data_set1 = arange(-4.9, -3, 0.1)
+
+
+def table_as_list(q_table: QTableWidget):
+    return [
+        float(data.text()) for data in
+        map(lambda i: q_table.item(i, 0), range(q_table.rowCount()))
+        if data or data.text
+    ]
 
 
 class MyMplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = plt.Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
 
-        self.compute_initial_figure()
-
-        #
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
         FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Maximum)
         FigureCanvas.updateGeometry(self)
+        self.bounds = 0.1
+        self.data_set = None
+        self.unique = set()
+        self.axes.set_xticks(arange(-10, 10, self.bounds))
 
-    def compute_initial_figure(self):
-        pass
+    def update_figure(self, data, plot_type):
 
-
-class FrequencePoligon(MyMplCanvas):
-    def __init__(self, *args, **kwargs):
-        MyMplCanvas.__init__(self, *args, **kwargs)
-
-    def compute_initial_figure(self):
-        self.axes.plot([0,1,2,3],[1,2,0,4],'r')
-
-    def update_figure(self):
+        if data == self.data_set:
+            return
+        self.data_set = data
+        self.data_set.sort()
         self.axes.clear()
-        self.axes.plot([i for i in range(4)])
-        self.draw()
+        data.sort()
 
+        if plot_type == 'empiric':
+            self.empiric()
+        elif plot_type == 'freq_pol':
+            self.freq_pol()
+        elif plot_type == 'cumulate':
+            self.cumulate()
 
+        self.axes.draw(renderer=None)
 
+    def freq_pol(self):
 
+        spread = (self.data_set[0] - self.bounds, self.data_set[-1] + self.bounds, self.bounds)
 
-def table_as_list(q_table: QTableWidget):
-    data_set = []
-    for i in range(0, q_table.rowCount()):
-        data = q_table.item(i, 0)
-        if data or data.text(): #TODO: ввід не працює норм
-            data_set.append(float(data.text()))
-    return data_set
+        self.axes.plot(unique(self.data_set+[spread[0], spread[1]]),
+                       [self.data_set.count(i) for i in unique(self.data_set+[spread[0], spread[1]])],
+                       "r")
+
+    def empiric(self):
+        probability = 1.0 / len(self.data_set)
+        emp_args = []
+        last_grow = self.data_set[0] - self.bounds
+        increase = float(0)  # звідки починати малювати графік
+        for i, j in map(lambda l: (self.data_set.count(l), l),
+                        unique(self.data_set +
+                               [self.data_set[0] - self.bounds, self.data_set[-1]+self.bounds])):
+            if i:
+                emp_args.extend(([last_grow, j], [increase, increase], 'g',
+                                 [j, j], [increase, increase + probability * i], 'r--'))
+                increase += probability * i
+                last_grow = j
+
+        emp_args.extend(([last_grow, last_grow + self.bounds], [1, 1], 'g'))
+        self.axes.plot(*emp_args)
+
+    def cumulate(self):
+        x = [self.data_set[0] - self.bounds]
+        y = [0]
+        for i, j in map(
+                        lambda l:
+                        (self.data_set.count(l), l),
+                        unique(self.data_set +
+                               [self.data_set[0] - self.bounds,
+                                self.data_set[-1] + self.bounds]
+                               )
+                        ):
+            if i:
+                x.append(j)
+                y.append(y[-1] + i)
+        self.axes.plot(x, y, 'b')
+
 
 class ApplicationWindow(QMainWindow):
     def __init__(self):
@@ -82,12 +130,13 @@ class ApplicationWindow(QMainWindow):
         sizePolicy.setHeightForWidth(self.data.sizePolicy().hasHeightForWidth())
         self.data.setSizePolicy(sizePolicy)
         self.data.setInputMethodHints(QtCore.Qt.ImhFormattedNumbersOnly)
-        self.data.setRowCount(5)
+        self.data.setRowCount(15)
+        self.set_default_data_set()
         self.data.setColumnCount(1)
 
         self.row_count = QDoubleSpinBox(self.main_widget)
         self.row_count.setDecimals(0)
-        self.row_count.setRange(5.0, 100.0)
+        self.row_count.setRange(15.0, 100.0)
         self.row_count.setProperty('value', 1.0)
         self.row_count.valueChanged.connect(self.row_count_change)
 
@@ -115,16 +164,13 @@ class ApplicationWindow(QMainWindow):
         main_h_layout.addLayout(data_layout)
 
         scroll_layout = QVBoxLayout(self.main_widget)
-        self.poligon = FrequencePoligon(self.main_widget)
+        self.poligon = MyMplCanvas(self.main_widget)
+        self.empiric = MyMplCanvas(self.main_widget)
+        self.cumulate = MyMplCanvas(self.main_widget)
 
-        self.get_button.clicked.connect(self.poligon.update_figure)
-
-        self.poligon1 = FrequencePoligon(self.main_widget)
-        self.poligon2 = FrequencePoligon(self.main_widget)
-       # self.poligon.
         scroll_layout.addWidget(self.poligon)
-        scroll_layout.addWidget(self.poligon1)
-        scroll_layout.addWidget(self.poligon2)
+        scroll_layout.addWidget(self.empiric)
+        scroll_layout.addWidget(self.cumulate)
 
         plots_scroll = QScrollArea(self.main_widget)
         plots_scroll.setLayout(scroll_layout)
@@ -136,19 +182,27 @@ class ApplicationWindow(QMainWindow):
 
         self.setCentralWidget(self.main_widget)
 
+    def set_default_data_set(self):
+        for i in range(self.data.rowCount()):
+            item = QTableWidgetItem()
+            item.setText(str(data_set1[i]))
+            self.data.setItem(i, 0, item)
+
     def row_count_change(self):
-        self.data_table.setRowCount(self.row_count.value())
+        self.data.setRowCount(self.row_count.value())
 
     def get_statistic(self):
         try:
             stat_data_set = table_as_list(self.data)
             if len(stat_data_set) < 2:
                 raise IOError('Not enough data')
-
+            self.poligon.update_figure(stat_data_set, 'freq_pol')
+            self.empiric.update_figure(stat_data_set, 'empiric')
+            self.cumulate.update_figure(stat_data_set, 'cumulate')
             self.output.setText(str(Lab1.get_data_set(stat_data_set)))
+
         except Exception as error:
             self.output.setText(error.args[0])
-
 
 
 app = QApplication(sys.argv)
